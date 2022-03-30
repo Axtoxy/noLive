45ea08355194942d708f9d77e25b7c84adcb8dca
Axtox <hag.here@gmail.com>
2022-03-30 18:03:24 +0300

    blog

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiKey: "AIzaSyBccWYRB8uvdyO8IqRjH-y54OOwdFw8PJo",
+  dbUrl: "https://nolivejournal-default-rtdb.europe-west1.firebasedatabase.app/"
+};
